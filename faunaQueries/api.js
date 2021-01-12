6a0ed8c109084b731e4a968baef84d6672fc7ceb
Kalo Pilato <kalo@abletech.nz>
2021-01-13 11:20:38 +1300

    refactor api clients

--- faunaQueries/api.js
+++ faunaQueries/api.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import faunadb from "faunadb";
 
 const { FAUNADB_SECRET: secret } = process.env;
@@ -17,58 +16,24 @@ const {
   Collection,
 } = faunadb.query;
 
-function getData(data) {
-  if (!data || data.errors) return null;
-  return data.data;
-}
-
-function getErrorMessage(error, data) {
-  if (error) return error.message;
-  if (data && data.errors) {
-    return data.errors[0].message;
-  }
-  return null;
-}
-
-export const useArticles = () => {
-  const [articles, setArticles] = useState();
-  const [error, setError] = useState();
-
-  if (!articles) {
-    fetchArticles()
-      .then((articles) => setArticles(articles))
-      .catch((error) => setError(error));
-  }
-
-  return {
-    data: articles,
-    errorMessage: null,
-    error: error,
-  };
-};
-
-//TODO better API clients
-export const createArticle = async (data) => {
+function _query(query) {
   if (!client) {
     throw new Error("Missing FaunaDB credentials");
   }
+  return client.query(query);
+}
 
-  return client.query(Create(Collection("articles"), { data }));
+export const createArticle = async (data) => {
+  return _query(Create(Collection("articles"), { data }));
 };
 
 export const fetchArticles = () => {
-  if (!client) {
-    throw new Error("Missing FaunaDB credentials");
-  }
-
-  return client
-    .query(
-      FaunaMap(
-        Paginate(Match(Index("all_articles"))),
-        Lambda("articles", Get(Var("articles")))
-      )
+  return _query(
+    FaunaMap(
+      Paginate(Match(Index("all_articles"))),
+      Lambda("articles", Get(Var("articles")))
     )
-    .then((response) => {
-      return JSON.stringify(response);
-    });
+  ).then((response) => {
+    return JSON.stringify(response);
+  });
 };
